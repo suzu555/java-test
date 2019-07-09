2d62fb92ad1d15c4ef16f3301054714550a96ee3
suzuki tatsuya japanease <47791097+suzu555@users.noreply.github.com>
2019-07-10 01:02:06 +0900

    import戻したコメントアウトで、スレッド化するか・・

--- Kwkwkw.java
+++ Kwkwkw.java
@@ -1,16 +1,33 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+//import java.io.BufferedOutputStream;
+//import java.io.FileOutputStream;
+//import java.io.FileReader;
 import java.io.File;
+//import java.io.OutputStreamWriter;
+//import java.nio.charset.Charset;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardOpenOption;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//import java.util.ResourceBundle;
+//import java.util.Properties;
 import java.io.FileInputStream;
+//import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.net.Socket;
+//import java.net.InetAddress;
 import java.net.ServerSocket;
+
 
 class Globalaa{
     static JTextArea area3;
